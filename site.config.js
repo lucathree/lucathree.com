276b190179bdaec28db988f2e6fa8003826ad534
lucathree <clee0627@gmail.com>
2023-06-18 14:33:41 +0900

    fix: 메인 블로그 설정 수정

--- site.config.js
+++ site.config.js
@@ -12,24 +12,24 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `개발 블로그`,
-      href: "https://lucathree.com",
-    },
+      name: '블로그 v1.0',
+      href: 'https://lucathree.github.io'
+    }
   ],
   // blog setting (required)
   blog: {
-    title: '루카쓰로그 Personal Edition',
-    description: '보다 개인적인 생각들을 담은 루카쓰로그의 서브 페이지입니다.',
-    theme: 'dark' // ['light', 'dark', 'auto']
+    title: '루카쓰로그 v2.0',
+    description: '개발자의 삶을 개발 중입니다',
+    theme: 'light' // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: 'https://personal.lucathree.com',
+  link: 'https://lucathree.com',
   since: 2021, // If leave this empty, current year will be used.
   lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ['노션', '블로그', '개발자', '일기', '감상'],
+    keywords: ['노션', '블로그', '개발자', '회고', '비전공자'],
   },
 
   // notion configuration (required)
